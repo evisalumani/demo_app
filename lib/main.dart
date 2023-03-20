7acf9d861e3f82705110fe1631104e27ae4a64ea
Evisa Lumani <evisa.lumani@bmw.de>
2023-03-20 16:19:31 +0100

    Change app title text

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Demo app'),
+      home: MyHomePage(title: 'Tutorial app'),
     );
   }
 }
